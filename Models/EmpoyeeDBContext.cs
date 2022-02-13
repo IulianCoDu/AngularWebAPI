f2f3175d7509984843455efc1a0da7bd69995dee
IulianCoDu <iulian.co.du@gmail.com>
2022-02-13 18:11:19 +0200

    Models created after the database with command: "Scaffold-DbContext "Server=DESKTOP-INOTF7E;Database=EmpoyeeDB;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models"

---
+++ Models/EmpoyeeDBContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AngularWebAPI.Models
+{
+    public partial class EmpoyeeDBContext : DbContext
+    {
+        public EmpoyeeDBContext()
+        {
+        }
+
+        public EmpoyeeDBContext(DbContextOptions<EmpoyeeDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Department { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-INOTF7E;Database=EmpoyeeDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.DepartmentId).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.DepartmentName)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.DateOfJoin).HasColumnType("date");
+
+                entity.Property(e => e.Department).HasMaxLength(500);
+
+                entity.Property(e => e.EmployeeName)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmpoyeeId).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.PhotoFileName)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
